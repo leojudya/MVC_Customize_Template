d04bbd29d7e51a1b62cf086db288994fc13c593e
Lin Zi Yuan <leojudya@gmail.com>
2023-06-13 17:51:06 +0800

    Remove bat

--- run.bat
+++
@@ -1,2 +0,0 @@
-robocopy "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\Extensions\Microsoft\Web\Mvc\Scaffolding\Templates" .\CodeTemplates /E
-robocopy ".\MvcView" .\CodeTemplates\MvcView "*.cs.t4" /E
